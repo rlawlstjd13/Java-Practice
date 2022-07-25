395d74869804f02ac53b0330a20677079b25116a
rlawlstjd13 <104603814+rlawlstjd13@users.noreply.github.com>
2022-07-25 17:55:12 +0900

    Add files via upload

---
+++ CapitalsDao.java
@@ -0,0 +1,17 @@
+package capitals;
+
+
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CapitalsDao {
+
+	int create(int number, String question, String answer, String continent) throws SQLException;
+
+	List<Capitals> read() throws SQLException;
+
+	int update(int number, String question, String answer, String continet) throws SQLException;
+
+	int delete(int number) throws SQLException;
+}
