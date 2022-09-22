18a8b9d6166422ff07b147bfbbc50d2653b61493
rlawlstjd13 <104603814+rlawlstjd13@users.noreply.github.com>
2022-09-22 16:39:06 +0900

    Add files via upload

---
+++ test/src/test/BackJoon5585.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.Scanner;
+
+public class BackJoon5585 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int[] coin = new int [] {500, 100, 50, 10, 5, 1};
+		
+		
+	}
+
+}
